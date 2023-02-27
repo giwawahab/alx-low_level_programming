01c5d965246603b777b89968eefc3ed4a0c94ca2
giwawahaba <giwawahabtomiwa2022@gmail.com>
2023-02-27 07:01:55 -0800

    task 5 hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function that the program will read first
+ *
+ * Return: this return value is always zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
