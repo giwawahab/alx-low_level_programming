311ef35adc904c6d7c7ed5a7dbf4a9b9fe0ddb51
giwawahaba <giwawahabtomiwa2022@gmail.com>
2023-02-27 06:48:23 -0800

    task 4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ *
+ *Return: always zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
