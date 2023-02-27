13eeb27f2b056fc308d08d6b9d0fdc805a7a625f
giwawahaba <giwawahabtomiwa2022@gmail.com>
2023-02-27 08:53:30 -0800

    task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - this function out to the standard erro
+ *
+ * Return: this return 1 of succes
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
