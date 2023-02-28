8f04afde9d7e90cf0e93c47fca3084d741724318
giwawahaba <giwawahabtomiwa2022@gmail.com>
2023-02-28 06:07:35 -0800

    task 2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: Always return zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
