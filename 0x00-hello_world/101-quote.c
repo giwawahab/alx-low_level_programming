52e97132e233feb43ed7e9a793fef654344fad61
giwawahaba <giwawahabtomiwa2022@gmail.com>
2023-02-28 03:17:59 -0800

    task 7 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - this function out to the standard erro
+ * main - this function out to the standard error
  *
  * Return: this return 1 of succes
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
