e305d32aa070b66f18fc1d93d1eb76dc385bb586
giwawahaba <giwawahabtomiwa2022@gmail.com>
2023-02-28 07:03:22 -0800

    task 8 print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This main function is run first
+ *
+ * Return: always return 0 if successful
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
