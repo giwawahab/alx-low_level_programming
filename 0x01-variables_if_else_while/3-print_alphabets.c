ceb50f8c75b672b505ed6106ce3315bb64999e60
giwawahaba <giwawahabtomiwa2022@gmail.com>
2023-02-28 06:15:31 -0800

    task 3 print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: return always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
