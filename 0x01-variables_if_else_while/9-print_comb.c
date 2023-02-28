3a476590b117bf42112ed672893212d1ffe6a848
giwawahaba <giwawahabtomiwa2022@gmail.com>
2023-02-28 07:18:07 -0800

    task 9 print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - the main function run first
+ *
+ * Return: Alway 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
